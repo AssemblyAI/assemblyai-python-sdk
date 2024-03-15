7017e69c39ba7459fe45d5b70e32ac843b1ce0b0
Patrick Loeber <98830383+ploeber@users.noreply.github.com>
2024-03-15 10:31:41 +0100

    feat(python/sdk): bump version to 0.23.1 (#4144)
    GitOrigin-RevId: 19c27a8458d57f0f01febf1fab9f9745bbf83eec

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ long_description = (Path(__file__).parent / "README.md").read_text()
 
 setup(
     name="assemblyai",
-    version="0.23.0",
+    version="0.23.1",
     description="AssemblyAI Python SDK",
     author="AssemblyAI",
     author_email="engineering.sdk@assemblyai.com",
