7d223cf05356a28dde138d82d97060fef0c0ea89
David E. Weekly <david@weekly.org>
2024-04-18 21:16:13 -0700

    Update assemblyai/types.py
    Co-authored-by: Patrick Loeber <98830383+ploeber@users.noreply.github.com>

--- assemblyai/types.py
+++ assemblyai/types.py
@@ -460,7 +460,7 @@ class RawTranscriptionConfig(BaseModel):
     redact_pii_audio: Optional[bool]
     "Generate a copy of the original media file with spoken PII 'beeped' out."
     redact_pii_audio_quality: Optional[RedactedAudioQuality]
-    "Set the quality level of the redacted audio: mp3 or wav"    
+    "The quality of the redacted audio file in case `redact_pii_audio` is enabled."    
     redact_pii_policies: Optional[List[PIIRedactionPolicy]]
     "The list of PII Redaction policies to enable."
     redact_pii_sub: Optional[PIISubstitutionPolicy]
