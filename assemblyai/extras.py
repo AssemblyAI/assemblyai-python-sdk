b58d325e5e5731a4ab838dd99651369196ef387a
Patrick Loeber <98830383+ploeber@users.noreply.github.com>
2024-09-07 15:18:53 +0200

    tests

--- assemblyai/extras.py
+++ assemblyai/extras.py
@@ -115,6 +115,7 @@ def stream_file(
             yield data
 
             time.sleep(chunk_duration)
+
 def file_from_stream(data: BinaryIO) -> str:
     """
     DeprecationWarning: `file_from_stream()` is deprecated and will be removed in 1.0.0. Use `Transcriber.upload_file()` instead.
