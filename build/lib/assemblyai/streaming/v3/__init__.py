a5677afae331cb97765be0818739ad42c58cce3f
jay2jp <jay2jp@hotmail.com>
2025-06-09 15:25:00 +0000

    added the missing parameter

---
+++ build/lib/assemblyai/streaming/v3/__init__.py
@@ -0,0 +1,27 @@
+from .client import StreamingClient
+from .models import (
+    BeginEvent,
+    EventMessage,
+    StreamingClientOptions,
+    StreamingError,
+    StreamingEvents,
+    StreamingParameters,
+    StreamingSessionParameters,
+    TerminationEvent,
+    TurnEvent,
+    Word,
+)
+
+__all__ = [
+    "BeginEvent",
+    "EventMessage",
+    "StreamingClient",
+    "StreamingClientOptions",
+    "StreamingError",
+    "StreamingEvents",
+    "StreamingParameters",
+    "StreamingSessionParameters",
+    "TerminationEvent",
+    "TurnEvent",
+    "Word",
+]
